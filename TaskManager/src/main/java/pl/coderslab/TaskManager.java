b22770372884f83b692104a696de5f343e88ad4e
pannorris <jakub.stefankiewicz@wp.pl>
2023-02-11 09:48:09 +0100

    workshop 2 test

--- TaskManager/src/main/java/pl/coderslab/TaskManager.java
+++ TaskManager/src/main/java/pl/coderslab/TaskManager.java
@@ -1,5 +1,13 @@
 package pl.coderslab;
 
+import java.io.PrintWriter;
+
 public class TaskManager {
-    //kod dwa
+    public static void main(String[] args) {
+//        File zadania=new File("tasks.csv");
+//        Scanner scan = new Scanner(zadania);
+        PrintWriter printWriter= new PrintWriter("test");
+
+    }
+
 }
---
+++ TaskManager2/src/main/java/pl/coderslab/TaskManager.java
@@ -0,0 +1,14 @@
+package pl.coderslab;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class TaskManager {
+
+    public static void main(String[] args) {
+        File zadania=new File("tasks.csv");
+        Scanner scan = new Scanner(zadania);
+
+    }
+}
